2780e9c857fcdfd248e70d3a45348a9a4460fc4a
bob <640297@qq.com>
2026-03-08 20:54:43 +0800

    feat:超过1年,自动处理开始时间

--- datafeed.py
+++ datafeed.py
@@ -72,15 +72,10 @@ class EdbDatafeed(BaseDatafeed):
         now = datetime.now(DB_TZ)
         req.start = req.start.astimezone(DB_TZ)
         days_from_start = (now - req.start).days
-        if req.interval == Interval.MINUTE:
-            if days_from_start > FREE_MODE_MINUTE_DATA_DAYS:
-                output(f"免费用户仅可获取最近 {FREE_MODE_MINUTE_DATA_DAYS} 天的分钟数据。您请求的起始日距今 {days_from_start} 天。日线数据可查询任意历史区间。")
-                return []
-            elif days_from_start == FREE_MODE_MINUTE_DATA_DAYS:
-                # 1分钟周期刚好满1年时，使用当前时间往前一年+1分钟作为起始时间
-                if period == 60:
-                    # 1分钟接口,只支持,当前时间前的1年内
-                    req.start = now - timedelta(days=FREE_MODE_MINUTE_DATA_DAYS) + timedelta(minutes=1)
+        if req.interval == Interval.MINUTE and days_from_start >= FREE_MODE_MINUTE_DATA_DAYS:
+            output(f"免费用户仅可获取最近 {FREE_MODE_MINUTE_DATA_DAYS} 天内的1分钟数据,自动处理开始时间。您请求的起始日距今 {days_from_start} 天。日线数据可查询任意历史区间。")
+            # 1分钟周期刚好满1年时，使用当前时间往前一年+1分钟作为起始时间
+            req.start = now - timedelta(days=FREE_MODE_MINUTE_DATA_DAYS) + timedelta(minutes=1)
 
         if req.symbol.endswith("888"):
             tq_symbol = f"KQ.m@{req.exchange.value}.{req.symbol.replace('888', '')}"
